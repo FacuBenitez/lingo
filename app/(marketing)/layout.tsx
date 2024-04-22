bdb748c38d2b61dea09c13f26750d78343463bfb
FacuBenitez <facu98ben@gmail.com>
2024-04-22 00:17:41 -0300

    layout terminado

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,20 +1,21 @@
-import { Header } from "./header"
+import { Footer } from "./footer";
+import { Header } from "./header";
 
 type Props = {
-    children: React.ReactNode,
-}
+  children: React.ReactNode;
+};
 
-
-const Marketinglayout = ( {children }:Props ) => {
+const MarketingLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen flex flex-col">    
-    <Header/>
-    <main className="flex-1 flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
         {children}
-    </main>
-
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Marketinglayout
+export default MarketingLayout;
+
